a34936e75934cb5f993e18d3b6db31be712987f5
Jamaludin <sanur@embreo.co>
2019-04-06 05:35:42 +0700

    Hide unnecessary UI

--- src/customApp/containers/Sidebar/index.js
+++ src/customApp/containers/Sidebar/index.js
@@ -1,17 +1,18 @@
 const options = [
-  {
-    key: 'githubSearch',
-    label: 'sidebar.githubSearch',
-    leftIcon: 'ion-social-github'
-  },
-  {
-    key: 'blank_page',
-    label: 'sidebar.blankPage',
-    leftIcon: 'ion-document'
-  },
+  // {
+  //   key: 'githubSearch',
+  //   label: 'sidebar.githubSearch',
+  //   leftIcon: 'ion-social-github'
+  // },
+  // {
+  //   key: 'blank_page',
+  //   label: 'sidebar.blankPage',
+  //   leftIcon: 'ion-document'
+  // },
   {
     key: 'arrival',
     label: 'sidebar.guestPortal',
+    leftIcon: 'ion-document',
   }
 ];
 export default options;
